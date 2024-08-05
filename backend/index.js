6be7ab2900c076669606ded06d00fbb494d83a15
Raj1367 <138358960+Raj1367@users.noreply.github.com>
2024-08-05 14:19:52 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const PORT = 8080 || process.env.PORT
 require('dotenv').config()
 
 app.use(cors({
-    origin: ["https://user-auth-frontend-drab.vercel.app"],
+    origin: "https://user-auth-frontend-drab.vercel.app",
     methods: ["POST","GET"],
     credentials: true
 }))
